3d0056a4f948b6ce39c10817f9c5265f907a47f5
SimonPolome <sim.polome@gmail.com>
2022-04-27 01:12:01 +0200

    commit

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,36 @@
-console.log('I am not a penguin');
+"use strict";
+
+const btnPrev = document.querySelector(".icon--prev");
+const btnNext = document.querySelector(".icon--next");
+
+btnNext.addEventListener("click", next)
+btnPrev.addEventListener("click", prev)
+
+function next(){
+    let elShow = document.querySelector(".page--show");
+    let elNext = elShow.nextElementSibling;
+    elShow.classList.remove("page--show");
+    if(elNext){
+        elNext.classList.add("page--show");
+    }else{
+        let elFirst = elShow.parentNode.firstElementChild;
+        elFirst.classList.add("page--show");
+    }    
+};
+function prev(){
+    let elShow = document.querySelector(".page--show");
+    let elPrev = elShow.previousElementSibling;
+    elShow.classList.remove("page--show");
+    if(elPrev){
+        elPrev.classList.add("page--show");
+    }else{
+        let elLast = elShow.parentNode.firstElementChild;
+        elLast.classList.add("page--show");
+    }    
+};
+
+const slider = document.querySelector("main");
+const hammerSlider = new Hammer(slider);
+
+hammerSlider.on("swipeleft", next);
+hammerSlider.on("swiperight", prev);
